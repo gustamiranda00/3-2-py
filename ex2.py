877aebe09e00529fac078af616e411e6ef2f7916
gustamiranda00 <gustavomiranda@colegiopaulodetarso.com.br>
2025-10-01 16:14:14 +0000

    git add

---
+++ ex2.py
@@ -0,0 +1,7 @@
+def verificar_saldo(valor):
+    if valor > 1000:
+        print("Saldo suficiente para investimento")
+    else:
+        print("Saldo insuficiente para investimento")
+
+verificar_saldo(1500)
